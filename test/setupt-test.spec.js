9f63206ac9996d2033e7a07e167c30e3bc58fd98
H (إتش ) <heshamscorpe55@gmail.com>
2018-04-03 21:23:32 +0200

    Fix bug

--- test/setupt-test.spec.js
+++ test/setupt-test.spec.js
@@ -7,7 +7,7 @@ describe('PRE-TEST Checklist', function() {
   it('NODE_ENV="TEST"', function() {
 
     const ERROR_MESSAGE = 'NODE_ENV must be set to test';
-    assert(NODE_ENV && NODE_ENV === 'production', ERROR_MESSAGE);
+    assert(NODE_ENV && NODE_ENV === 'test', ERROR_MESSAGE);
 
   });
 
