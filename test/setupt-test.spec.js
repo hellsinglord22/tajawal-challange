e943ff684c9176b195b6cfb58210f8e2640598b4
H (إتش ) <heshamscorpe55@gmail.com>
2018-04-03 21:20:39 +0200

    add test and travis support

---
+++ test/setupt-test.spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert'),
+  { NODE_ENV } = process.env;
+
+
+describe('PRE-TEST Checklist', function() {
+
+  it('NODE_ENV="TEST"', function() {
+
+    const ERROR_MESSAGE = 'NODE_ENV must be set to test';
+    assert(NODE_ENV && NODE_ENV === 'production', ERROR_MESSAGE);
+
+  });
+
+});
